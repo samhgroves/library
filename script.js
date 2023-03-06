1a7db053cd17524950af9e8e2883957324121360
Sam Groves <samuelhgroves@gmail.com>
2023-03-06 12:34:53 -0500

    User can now add books and a div is created only for new and unique entries

--- script.js
+++ script.js
@@ -12,26 +12,37 @@ Book.prototype.sayTitle = function () {
 function addBookToLibrbary() {
   myLibrary.push();
 }
-
+/*
 const bookOne = new Book("Harry Potter", "JK Rowling");
 const bookTwo = new Book("Catcher in the Rye", "Some Guy");
 const bookThree = new Book("monkeyooke", "samgroves");
 myLibrary.push(bookOne);
 myLibrary.push(bookTwo);
 myLibrary.push(bookThree);
-
+*/
 function displayBooks() {
   const bookList = document.getElementById("book-list");
-
+  // Check if the book is already in the DOM
   myLibrary.forEach(function (book) {
-    const aBook = document.createElement("div");
-    aBook.textContent = book.title + " by " + book.author;
-    bookList.appendChild(aBook);
+    const bookExists = Array.from(bookList.children).some(function (element) {
+      return element.textContent === book.title + " by " + book.author;
+    });
+    // If book is not in DOM, add it
+    if (!bookExists) {
+      const bookDiv = document.createElement("div");
+      bookDiv.textContent = book.title + " by " + book.author;
+      bookList.appendChild(bookDiv);
+    }
   });
 }
 displayBooks();
-
 const submitBtn = document.getElementById("submit");
 submitBtn.addEventListener("click", function () {
-  console.log("hi");
+  let newTitle = document.getElementById("title");
+  let newAuthor = document.getElementById("author");
+  let newBook = new Book(newTitle.value, newAuthor.value);
+  myLibrary.push(newBook);
+  displayBooks();
+  newTitle.value = "";
+  newAuthor.value = "";
 });
