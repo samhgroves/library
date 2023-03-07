012622eeb3e60535ee66d6345ccb66e3b4e82833
Sam Groves <samuelhgroves@gmail.com>
2023-03-06 20:57:43 -0500

    Added delete button

--- script.js
+++ script.js
@@ -5,44 +5,44 @@ function Book(title, author) {
   this.author = author;
 }
 
-Book.prototype.sayTitle = function () {
-  console.log(this.title);
-};
-
-function addBookToLibrbary() {
-  myLibrary.push();
-}
-/*
-const bookOne = new Book("Harry Potter", "JK Rowling");
-const bookTwo = new Book("Catcher in the Rye", "Some Guy");
-const bookThree = new Book("monkeyooke", "samgroves");
-myLibrary.push(bookOne);
-myLibrary.push(bookTwo);
-myLibrary.push(bookThree);
-*/
 function displayBooks() {
   const bookList = document.getElementById("book-list");
   // Check if the book is already in the DOM
-  myLibrary.forEach(function (book) {
+  myLibrary.forEach(function (book, index) {
     const bookExists = Array.from(bookList.children).some(function (element) {
-      return element.textContent === book.title + " by " + book.author;
+      return element.dataset.bookIndex === String(index);
     });
     // If book is not in DOM, add it
     if (!bookExists) {
       const bookDiv = document.createElement("div");
       bookDiv.textContent = book.title + " by " + book.author;
+      bookDiv.dataset.bookIndex = index;
       bookList.appendChild(bookDiv);
+      // Add delete button to each book div
+      const deleteBtn = document.createElement("BUTTON");
+      deleteBtn.textContent = "Delete";
+      bookDiv.appendChild(deleteBtn);
+      deleteBtn.addEventListener("click", function () {
+        const bookIndex = parseInt(bookDiv.dataset.bookIndex);
+        myLibrary.splice(bookIndex, 1);
+        bookList.removeChild(bookDiv);
+      });
     }
   });
 }
-displayBooks();
+
 const submitBtn = document.getElementById("submit");
 submitBtn.addEventListener("click", function () {
   let newTitle = document.getElementById("title");
   let newAuthor = document.getElementById("author");
   let newBook = new Book(newTitle.value, newAuthor.value);
-  myLibrary.push(newBook);
-  displayBooks();
+  const bookExists = myLibrary.some(function (book) {
+    return book.title === newBook.title && book.author === newBook.author;
+  });
+  if (!bookExists) {
+    myLibrary.push(newBook);
+    displayBooks();
+  }
   newTitle.value = "";
   newAuthor.value = "";
 });
